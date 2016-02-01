d22093cb02b28c02613edb2e4e3b1247e07e1342
Scott Murphy <scottmurphy09@gmail.com>
2016-01-31 21:26:46 -0600

    Time to watch last days of disco

--- device_driver_example.c
+++ device_driver_example.c
@@ -1,5 +1,5 @@
 /* Device driver tutorial 
-http://tldp.org/LDP/lkmpg/2.6/html/lkmpg.html#AEN181
+   http://tldp.org/LDP/lkmpg/2.6/html/lkmpg.html#AEN181
 */
 
 /*  
@@ -13,33 +13,6 @@ http://tldp.org/LDP/lkmpg/2.6/html/lkmpg.html#AEN181
 
 #define DRIVER_AUTHOR "Scott Murphy <scott.murphy@plowtech.net>" 
 #define DRIVER_DESC "A sample driver"
-
-
-static short int myshort = 1;
-static int myint = 420;
-static long int mylong = 9999;
-static char *mystring = "blah";
-static int myintArray[2] = { -1, -1 };
-static int arr_argc = 0;
-/* 
- * module_param(foo, int, 0000)
- * The first param is the parameters name
- * The second param is it's data type
- * The final argument is the permissions bits, 
- * for exposing parameters in sysfs (if non-zero) at a later stage.
- */
-module_param(myshort, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-MODULE_PARM_DESC(myshort, "A short integer");
-module_param(myint, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-MODULE_PARM_DESC(myint, "An integer");
-module_param(mylong, long, S_IRUSR);
-MODULE_PARM_DESC(mylong, "A long integer");
-module_param(mystring, charp, 0000);
-MODULE_PARM_DESC(mystring, "A character string");
-
-
-
-
 
 
 
@@ -68,23 +41,88 @@ MODULE_SUPPORTED_DEVICE("testdevice");
 
 
 
-int hello_2_init(void)
-{
-	printk(KERN_INFO "Hello world 1.\n");
 
-	/* 
-	 * A non 0 return means init_module failed; module can't be loaded. 
-	 */
-	return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static short int myshort = 1;
+static int myint = 420;
+static long int mylong = 9999;
+static char *mystring = "blah";
+static int myintArray[2] = { -1, -1 };
+static int arr_argc = 0;
+/* 
+ * module_param(foo, int, 0000)
+ * The first param is the parameters name
+ * The second param is it's data type
+ * The final argument is the permissions bits, 
+ * for exposing parameters in sysfs (if non-zero) at a later stage.
+ */
+module_param(myshort, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(myshort, "A short integer");
+module_param(myint, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+MODULE_PARM_DESC(myint, "An integer");
+module_param(mylong, long, S_IRUSR);
+MODULE_PARM_DESC(mylong, "A long integer");
+module_param(mystring, charp, 0000);
+MODULE_PARM_DESC(mystring, "A character string");
+
+
+
+/*
+ * module_param_array(name, type, num, perm);
+ * The first param is the parameter's (in this case the array's) name
+ * The second param is the data type of the elements of the array
+ * The third argument is a pointer to the variable that will store the number 
+ * of elements of the array initialized by the user at module loading time
+ * The fourth argument is the permission bits
+ */
+module_param_array(myintArray, int, &arr_argc, 0000);
+MODULE_PARM_DESC(myintArray, "An array of integers");
+
+
+
+
+
+
+
+
+int hello_init(void)
+{
+  printk(KERN_INFO "Hello world !.\n");
+        
+
+  /* 
+   * A non 0 return means init_module failed; module can't be loaded. 
+   */
+  int i;
+  printk(KERN_INFO "Hello,world 5 \n=====================================\n");
+  return 0;
 }
 
-void hello_2_exit(void)
+void hello_exit(void)
 {
-	printk(KERN_INFO "Goodbye world 1.\n");
+  printk(KERN_INFO "Goodbye world !.\n");
 }
 
 
 
-module_init(hello_2_init);
-module_exit(hello_2_exit);
+module_init(hello_init);
+module_exit(hello_exit);
 
