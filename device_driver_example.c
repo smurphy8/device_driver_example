43ff0b0ed6c2d2f5920b79f68dab336da50dd0ba
Scott Murphy <scottmurphy09@gmail.com>
2016-01-31 21:09:51 -0600

    Moved some stuff around

--- device_driver_example.c
+++ device_driver_example.c
@@ -8,8 +8,36 @@ http://tldp.org/LDP/lkmpg/2.6/html/lkmpg.html#AEN181
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h> /* Needed for the Macros */
+#include <linux/moduleparam.h> /* command line args */
+#include <linux/stat.h>
+
 #define DRIVER_AUTHOR "Scott Murphy <scott.murphy@plowtech.net>" 
-#define Driver_DESC "A sample driver"
+#define DRIVER_DESC "A sample driver"
+
+
+/*  
+ *  You can use strings, like this:
+ */
+
+/* 
+ * Get rid of taint message by declaring code as GPL. 
+ */
+MODULE_LICENSE("GPL");
+
+/*
+ * Or with defines, like this:
+ */
+MODULE_AUTHOR(DRIVER_AUTHOR);	/* Who wrote this module? */
+MODULE_DESCRIPTION(DRIVER_DESC);	/* What does this module do */
+
+/*  
+ *  This module uses /dev/testdevice.  The MODULE_SUPPORTED_DEVICE macro might
+ *  be used in the future to help automatic configuration of modules, but is 
+ *  currently unused other than for documentation purposes.
+ */
+MODULE_SUPPORTED_DEVICE("testdevice");
+
+
 
 int hello_2_init(void)
 {
@@ -30,3 +58,4 @@ void hello_2_exit(void)
 
 module_init(hello_2_init);
 module_exit(hello_2_exit);
+
